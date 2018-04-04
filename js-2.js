24efd28873ce58630b78509c7a914e9e44bf501a
lukejohnlayson <30330558+lukejohnlayson@users.noreply.github.com>
2018-04-04 19:43:23 +1200

    Update js-2.js

--- js-2.js
+++ js-2.js
@@ -1,3 +1,4 @@
+// Javascript for experimental nav bar
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
@@ -6,4 +7,12 @@ function openNav() {
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
+}
+// Javascript for copyright overlay
+function on() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function off() {
+    document.getElementById("overlay").style.display = "none";
 }
