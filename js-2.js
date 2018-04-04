d829d0958cd8733e9aae323cf17383b0f9ace57a
lukejohnlayson <30330558+lukejohnlayson@users.noreply.github.com>
2018-04-04 19:44:40 +1200

    Update js-2.js

--- js-2.js
+++ js-2.js
@@ -8,11 +8,4 @@ function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
 }
-// Javascript for copyright overlay
-function on() {
-  document.getElementById("overlay").style.display = "block";
-}
 
-function off() {
-    document.getElementById("overlay").style.display = "none";
-}
